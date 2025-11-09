5ac9a2659b5bd00f720394293a49a4e581d3f713
Keff-dev <kay.dev.effendi@gmail.com>
2025-11-09 11:04:03 -0500

    logic fixes

--- src/main.asm
+++ src/main.asm
@@ -1,17 +1,4 @@
 
-; Include code for finite sets, write code that calculates the intersections installed
-; Main idea: Have side functions that replace a proof with each step along the way to prove what we're proving.
- 
-; Task one, get divisiider to work!
-; first letter check,
-; e [158] || o [168]
-; m [166] || d [157] || a [154] || s[172] || n [167] || pow [169]
-; check is e || o
-; Prints true or false
-
-; Even tree:
-; e -> mult(e)
-
 include 'include/ez80.inc'
 include 'include/tiformat.inc'
 include 'include/ti84pceg.inc'
@@ -30,7 +17,7 @@ main:
     xor  A,A
     ld   (ti.curUnder), A
     ld   B,(iy+9)
-	ld	 c,(iy+28)				;back up old flag values so StringInput doesn't break screen
+	ld	 c,(iy+28)				; back up old flag values so StringInput doesn't break screen
 	res	 6,(iy+28)
 	set	 7,(iy+9)
 	push bc
@@ -40,29 +27,17 @@ main:
     call ti.PutS                ; puts the string grabbed on screen
     
     ld a, h                     ; assuming string is in hl and firsts byte is the first character of the string
-    cp 158                      ; Check if byte of stringInput matches for e [BUT ACTUALLY DOESN'T BECAUSE WHY WOULD IT!!]
-    jp nz, even                 ; when string is "EVEN([anything])", still skipping over jump...
-    ; Lowkey goes to even no mater what- HAVE TO FIX BF PREZ
+    cp 158                      ; Check if byte of stringInput matches for E
+    jp nz, even                 
     
     cp 168
-    jp z, odd
-
-    call ti.HomeUp              ; Print 2nd phase of function
-    ld hl, inputSndP
-    call ti.PutS
-
-
-    
-
-
+    jp z, odd                   ; Jumps to odd code 
 
 
     RET
 even:
-    call ti.NewLine
-
     call ti.NewLine             ; Moves to phase 3
-    ld   hl, inputSndP
+    ld   hl, inputPrompt
 	ld	 de, ti.ioPrompt
     ldir
     xor  A,A
@@ -79,7 +54,6 @@ even:
 
     cp 166                      ; Supposed to check for "M"
     jp nz, multt
-    ; Lowkey goes to even no mater what- HAVE TO FIX BF PREZ
 
 multt:                          ; Would be the ends of checks because 2k*c would still be true that 2|2kc
     ld hl, 0                    ; Hoping resetting hl would stop it from crashing
@@ -91,21 +65,21 @@ multt:                          ; Would be the ends of checks because 2k*c would
 
     ret
     
-odd:  ; Still needs to be worked through, same thing with all the jp zn, even/multt things
+odd:  
     call ti.HomeUp              ; Print 2nd phase of function
-    ld hl, inputSndP
+    ld hl, inputPrompt
     call ti.PutS
 
+    cp 169                      ; Supposed to check for another char
+    jp nz, multt
 
 inputPrompt:
     db "Function: ", 0
 inputPrompt_end:
-inputSndP:
-    db "Appling Opperation: ", 0 ; Expecting, addition, muliplication, subtraction, etc...
 true:
-    db "Proof is true", 0 
+    db "True", 0 
 false:
-    db "Proof is false", 0
+    db "False", 0
 
 
 ; While not the enter key, let user keep typing.
